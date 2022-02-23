38b9fdf6534bbaa148b5c0393c3569032f0d3cce
HunterJarvis <70108814+Hunter-Jarvis15@users.noreply.github.com>
2022-02-22 20:32:38 -0700

    Add files via upload

---
+++ HW5/script.js
@@ -0,0 +1,44 @@
+var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10"];
+
+var blankImagePath = "images/Block.png";
+
+var actualImages = new Array();
+
+function blocks()
+{
+    createRandomImageArray();
+    for(var i = 0; i < imageTags.length; i++)
+    {
+        document.getElementById(imageTags[i]).src= blankImagePath;
+    }
+}
+
+function createRandomImageArray()
+{
+    var actualImagePath = ["images/Mario.png", "images/Luigi.png", "images/Toad.png", "images/Peach.png", "images/Bowser.png"];
+    // create another array to make sure the images only get added twice
+    var count = [0,0];
+    // create a while statement to check to see if our actual image array is full
+    while(actualImages.length < 4)
+    {
+        // get a random number between 0 and the number total number of images that we can choose from
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+          // create an if statement that says if the total number added is less than 2, then
+        // add the image to the actual image array
+        if(count[randomNumber] < 2)
+        {
+            actualImages.push(actualImagePath[randomNumber]);
+            // then add one to the array that makes sure only two images can be added
+            count[randomNumber] = count[randomNumber] + 1;
+        }
+    }    
+}
+
+function flipImage(number)
+{
+    document.getElementById(imageTags[number]).src= actualImages[number];
+        // this should be a quick function that just changes
+        // the image based on what number was pressed
+        
+    
+}
